94e88663a7ee2261080eef9d8bf50501a1d51387
Panchito Rey <pancheps@hotmail.com>
2022-05-12 14:54:33 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bills
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
